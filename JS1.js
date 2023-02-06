1cb395f41585a8781ca9e5dda4b42c95d172c14d
ga-machado <86075994+ga-machado@users.noreply.github.com>
2023-02-06 14:05:22 -0300

    Add files via upload

---
+++ JS1.js
@@ -0,0 +1,5 @@
+let entrada = prompt("Seja bem vindo! Digite seu nome")
+let saida  = prompt(entrada + ", obrigado por corrigir essa atividade do curso de JS. Quantas atividades você corrigiu até agora (digite um número)?")
+let numeral = parseInt(saida) 
+let soma = numeral + 1
+alert("Legal! Se você corrigiu " + saida + " atividades, com essa você corrigiu " + soma + " atividades ao todo! Espero que não faltem muitas mais.")
